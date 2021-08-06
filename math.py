ed4826e50734e956a87d08c838d1bb2e567069d1
omkarjamdar <omkar_jamdar@persistent.com>
2021-08-06 17:16:28 +0530

    Implemented multiply fn() from local repo

--- math.py
+++ math.py
@@ -3,10 +3,14 @@ def add(x,y):
  return x+y
 # Substract implementation
 def substract(x,y):
+<<<<<<< HEAD
  return x-y
+=======
+ return x-y                     #perform on branch Bug456
+>>>>>>> 1a3459c (Implemented multiply fn() from local repo)
 # Mulriply implementation
 def multiply(x,y):
- pass
+ return x*y                     #from local repo.
 
 # Divide implementation
 def divide(x,y):
